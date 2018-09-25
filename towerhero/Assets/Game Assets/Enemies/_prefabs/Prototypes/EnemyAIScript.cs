7200158c347cbef226317a5317f8086749c40989
ben <b.mcgarvey.bm@gmail.com>
2018-09-26 01:01:07 +1000

    restrict navmesh to enemy area and add script to add navagent to enemies at run time

---
+++ towerhero/Assets/Game Assets/Enemies/_prefabs/Prototypes/EnemyAIScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIScript : MonoBehaviour {
+
+	GameObject goal;
+	UnityEngine.AI.NavMeshAgent AIAgent;
+
+	void Awake() {
+		gameObject.AddComponent<UnityEngine.AI.NavMeshAgent>();
+		AIAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+
+	void Start () {
+		goal = GameObject.FindWithTag("EnemyGoal");
+		AIAgent.SetDestination(goal.transform.position);
+	}
+}
