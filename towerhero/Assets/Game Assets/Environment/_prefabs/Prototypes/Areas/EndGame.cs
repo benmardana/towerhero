b4f286e82d69f21f7ceca41e3a7b5ba11c23a6cd
ben <b.mcgarvey.bm@gmail.com>
2018-09-27 14:39:43 +1000

    Presents gameover and resets scene after 5 seconds if too many enemies get through

--- towerhero/Assets/Game Assets/Environment/_prefabs/Prototypes/Areas/EndGame.cs
+++ towerhero/Assets/Game Assets/Environment/_prefabs/Prototypes/Areas/EndGame.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class EndGame : MonoBehaviour {
@@ -8,6 +9,7 @@ public class EndGame : MonoBehaviour {
 	int health = 3;
 
 	public Canvas gameOverScreen;
+	public int time = 5;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,6 +20,20 @@ public class EndGame : MonoBehaviour {
 		if (health <= 0){
 			// end game
 			Instantiate(gameOverScreen);
+			// not currently working - only for testing really
+			// RestartTimer timer = (RestartTimer) gameOverScreen.gameObject.GetComponentInChildren(typeof(RestartTimer));
+			// timer.SetRestartTimer(time);
+			Scene loadedLevel = SceneManager.GetActiveScene();
+     		StartCoroutine(ReloadScene(loadedLevel));
 		}
+	}
+
+	IEnumerator ReloadScene(Scene level){
+		yield return new WaitForSeconds(time);
+		SceneManager.LoadScene (level.buildIndex);
+	}
+
+	public void reduceHealth(int damage){
+		health -= damage;
 	}
 }
