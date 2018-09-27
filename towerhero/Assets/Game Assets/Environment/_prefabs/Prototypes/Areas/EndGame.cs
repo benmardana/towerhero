184ab93351b2ac7dcb69c770fd27c253d1a49936
ben <b.mcgarvey.bm@gmail.com>
2018-09-27 13:39:39 +1000

    add gameover screen prefab and link to endgame area

---
+++ towerhero/Assets/Game Assets/Environment/_prefabs/Prototypes/Areas/EndGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+	public int maxHealth = 3;
+	int health = 3;
+
+	public Canvas gameOverScreen;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0){
+			// end game
+			Instantiate(gameOverScreen);
+		}
+	}
+}
