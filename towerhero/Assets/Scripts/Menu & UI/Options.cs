58a5aa89cfaf93e2fdec3def06f59f48bbdecef1
Daniel Sutherland <danielmail40@gmail.com>
2018-09-28 21:54:34 +1000

    Menu screens, buttons, difficulty

---
+++ towerhero/Assets/Scripts/Menu & UI/Options.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Options {
+    public static float difficulty = 0.0f;	// used to set enemy movement speed
+}
