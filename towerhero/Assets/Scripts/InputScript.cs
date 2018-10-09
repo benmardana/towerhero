577ee23ec407b881b0dcf946584eb83ae2f79b42
ben <b.mcgarvey.bm@gmail.com>
2018-10-09 18:57:45 +1100

    fixing turret placement

--- towerhero/Assets/Scripts/InputScript.cs
+++ towerhero/Assets/Scripts/InputScript.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class InputScript : MonoBehaviour {
@@ -36,18 +37,19 @@ public class InputScript : MonoBehaviour {
                 if (ResourceManager.resources >= 50)
                 {
 	                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	                Debug.DrawRay(ray.origin, ray.direction, Color.red);
 	                var hits = Physics.RaycastAll(ray.origin, ray.direction, 2000f);
-	                foreach (var hit in hits)
+	                var terrainHits = hits.Where(x => x.collider.CompareTag("Terrain"));
+	                var placeableHits = hits.Where(x => x.collider.CompareTag("Placeable"));
+	                var nonPlaceableHits = hits.Where(x => x.collider.CompareTag("NonPlaceable"));
+	                if (placeableHits.Any() && !nonPlaceableHits.Any())
 	                {
-		                if (hit.collider.tag == "Wall")
-		                {
-			                Debug.Log("Found the layer\n");
-                            GameObject turret = Turrets[0];
-                            // This ensures the turret is loaded 'above' the wall.
-                            Vector3 instantiationPoint = new Vector3(hit.point.x, hit.point.y + turret.transform.position.y, hit.point.z);
-                            Instantiate(turret, instantiationPoint, Quaternion.identity);
-                            ResourceManager.TurretBuilt();
-		                }
+		                var hit = terrainHits.First();
+		                Debug.Log(terrainHits.First().point);
+		                GameObject turret = Turrets[0];
+		                Vector3 instantiationPoint = new Vector3(hit.point.x + 1.6f, hit.point.y + turret.transform.position.y, hit.point.z);
+		                Instantiate(turret, instantiationPoint, Quaternion.identity);
+		                ResourceManager.TurretBuilt();
 	                }
                 }
             }
