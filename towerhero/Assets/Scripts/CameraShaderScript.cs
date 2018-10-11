92e2f8b9f39b2efffcb6ce2a9cdb85e22c51dfb2
Daniel Sutherland <danielmail40@gmail.com>
2018-10-11 21:45:41 +1100

    Toon Shader, almost done

---
+++ towerhero/Assets/Scripts/CameraShaderScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaderScript : MonoBehaviour {
+
+    public Shader shader;
+    
+	void Start () {
+        GetComponent<Camera>().SetReplacementShader(shader, "");
+    }
+}
