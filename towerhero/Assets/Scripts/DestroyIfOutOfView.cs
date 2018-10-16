7deb2a337fe46d6abb12211e42c95bcb012d089f
Daniel Sutherland <danielmail40@gmail.com>
2018-10-16 23:15:27 +1100

    projectile out of view implemented

---
+++ towerhero/Assets/Scripts/DestroyIfOutOfView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIfOutOfView : MonoBehaviour {
+
+    // If the projectile is not visible, destroy it
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
